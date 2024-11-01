791ce14935c936c8b03b9bfdad842e667375db5a
vinderet <vinderet@ya.ru>
2024-11-01 18:40:27 +0300

    сделали GUI, отобразили поле ввода, кнопки, оптимизировали кнопки, вынеся их в отдельную функцию

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print('Привет 201 группа')
+import tkinter as tk
+
+
+def add_digit(digit):
+    value = calc.get() + str(digit)
+    calc.delete(0, tk.END)
+    calc.insert(0, value)
+
+
+def make_digit_button(digit):
+    return tk.Button(text=digit, bd=5, font=('Arial', 13), command=lambda: add_digit(digit))
+
+win = tk.Tk()
+win.geometry(f"240x260+100+200")
+win["bg"] = "#9FE2BF"
+win.title('Калькулятор')
+
+calc = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 15), width=15)
+calc.grid(row=0, column=0, columnspan=3, stick='we')
+
+make_digit_button('1').grid(row=1, column=0, stick='wens', padx=5, pady=5)
+make_digit_button('2').grid(row=1, column=1, stick='wens', padx=5, pady=5)
+make_digit_button('3').grid(row=1, column=2, stick='wens', padx=5, pady=5)
+make_digit_button('4').grid(row=2, column=0, stick='wens', padx=5, pady=5)
+make_digit_button('5').grid(row=2, column=1, stick='wens', padx=5, pady=5)
+make_digit_button('6').grid(row=2, column=2, stick='wens', padx=5, pady=5)
+make_digit_button('7').grid(row=3, column=0, stick='wens', padx=5, pady=5)
+make_digit_button('8').grid(row=3, column=1, stick='wens', padx=5, pady=5)
+make_digit_button('9').grid(row=3, column=2, stick='wens', padx=5, pady=5)
+make_digit_button('0').grid(row=4, column=0, stick='wens', padx=5, pady=5)
+
+win.grid_columnconfigure(0, minsize=60)
+win.grid_columnconfigure(1, minsize=60)
+win.grid_columnconfigure(2, minsize=60)
+
+win.grid_rowconfigure(1, minsize=60)
+win.grid_rowconfigure(2, minsize=60)
+win.grid_rowconfigure(3, minsize=60)
+win.grid_rowconfigure(4, minsize=60)
+
+
+
+
+
+
+win.mainloop()
+
+
